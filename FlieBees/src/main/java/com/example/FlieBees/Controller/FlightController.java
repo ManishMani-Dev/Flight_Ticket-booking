42bf766b9904a04b12673438ec25ba3ed743bbed
ManishMani-Dev <113405448+ManishMani-Dev@users.noreply.github.com>
2023-09-19 12:42:57 +0530

    Update FlightController.java

--- FlieBees/src/main/java/com/example/FlieBees/Controller/FlightController.java
+++ FlieBees/src/main/java/com/example/FlieBees/Controller/FlightController.java
@@ -3,6 +3,9 @@ package com.example.FlieBees.Controller;
 import com.example.FlieBees.DTOs.TripDetailsDto;
 import com.example.FlieBees.DTOs.TripFetchRequest;
 import com.example.FlieBees.Service.FlightFetchService;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,28 +14,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/flight")
 public class FlightController {
     @Autowired
     private FlightFetchService flightFetchService;
-    //TODO change return type
+    private Logger logger = LoggerFactory.getLogger(FlightFetchService.class);
     @GetMapping("/search")
     List<TripDetailsDto> searchFlights(@RequestParam("src") String source,
                                        @RequestParam("dst") String destination,
-                                       @RequestParam(name="stime",required=false) Timestamp startTime,
-                                       @RequestParam(name="etime",required=false)Timestamp endTime){
+                                       @RequestParam(name="stime",required=false) @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS",timezone="IST") Timestamp startTime,
+                                       @RequestParam(name="etime",required=false) @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS",timezone="IST")Timestamp endTime) throws ExecutionException, InterruptedException {
         TripFetchRequest tripFetch= TripFetchRequest.builder()
                 .source(source)
                 .destination(destination)
                 .startTime(startTime)
                 .endTime(endTime)
                 .build();
+        long s = System.currentTimeMillis();
         List<TripDetailsDto> res = flightFetchService.findTrips(tripFetch);
-//        List<TripDetailsDto> res = flightFetchService.findTripsAsync(tripFetch); /**Async**/
-        res.add(TripDetailsDto.builder()
-                .tripId(5555).build());
+        logger.info("normal call time taken {}",(System.currentTimeMillis()-s));
+//        s=System.currentTimeMillis();
+//        res = flightFetchService.findTripsExServ(tripFetch); /**ExecutorService**/
+//        logger.info("Executor Service call time taken {}",(System.currentTimeMillis()-s));
+        s=System.currentTimeMillis();
+        res.addAll( flightFetchService.findTripsAsync(tripFetch)); /**Async**/
+        logger.info("Completable future Service call time taken {}",(System.currentTimeMillis()-s));
         return res;
     }
 
